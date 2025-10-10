44a42a7fb715b018abb5f48b1cec41006da93824
Wojtek Zrałek <wojtek.zralek@icloud.com>
2025-10-10 20:26:42 +0200

    Remove posthog rewrites

--- next.config.ts
+++ next.config.ts
@@ -29,20 +29,6 @@ const nextConfig: NextConfig = {
             },
         ];
     },
-    async rewrites() {
-        return [
-            {
-                source: "/skibidi/static/:path*",
-                destination: "https://eu-assets.i.posthog.com/static/:path*",
-            },
-            {
-                source: "/skibidi/:path*",
-                destination: "https://eu.i.posthog.com/:path*",
-            },
-        ];
-    },
-    // This is required to support PostHog trailing slash API requests
-    skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
